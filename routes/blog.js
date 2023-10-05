09c9844bbf285523adc213dd82cc566e99227da0
Alin Triff <triffalin@gmail.com>
2023-10-05 17:28:34 +0300

    ejs and handled async code errors

--- routes/blog.js
+++ routes/blog.js
@@ -51,11 +51,19 @@ router.post('/posts', async function (req, res) {
 });
 
 router.get('/posts/:id', async function (req, res) {
-	const postId = req.params.id;
+	let postId = req.params.id;
+
+	try {
+		postId = new ObjectId(postId);
+	} catch (error) {
+		return res.status(404).render('404');
+		// return next(error);
+	}
+
 	const post = await db
 		.getDb()
 		.collection('posts')
-		.findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+		.findOne({ _id: postId }, { summary: 0 });
 
 	if (!post) {
 		return res.status(404).render('404');
