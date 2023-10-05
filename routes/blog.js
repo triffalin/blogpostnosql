7ef268aa737dffb6400d83507fbc5c0721026e2c
Alin Triff <triffalin@gmail.com>
2023-10-05 09:21:51 +0300

    inserted documents(new post)

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 const db = require('../data/database');
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -15,6 +18,29 @@ router.get('/posts', function (req, res) {
 router.get('/new-post', async function (req, res) {
 	const authors = await db.getDb().collection('authors').find().toArray();
 	res.render('create-post', { authors: authors });
+});
+
+router.post('/posts', async function (req, res) {
+	const authorId = new ObjectId(req.body.author);
+	const author = await db
+		.getDb()
+		.collection('authors')
+		.findOne({ _id: authorId });
+
+	const newPost = {
+		title: req.body.title,
+		summary: req.body.summary,
+		content: req.body.content,
+		date: new Date(),
+		author: {
+			id: authorId,
+			name: author.name,
+			email: author.email,
+		},
+	};
+
+	const result = await db.getDb().collection('posts').insertOne(newPost);
+	res.redirect('/posts');
 });
 
 module.exports = router;
