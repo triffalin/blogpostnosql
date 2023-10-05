6e2c9bbd737d3fdd5a09b88232cc500c857e2c19
Alin Triff <triffalin@gmail.com>
2023-10-05 15:28:26 +0300

    updated dcuments

--- routes/blog.js
+++ routes/blog.js
@@ -36,7 +36,7 @@ router.post('/posts', async function (req, res) {
 	const newPost = {
 		title: req.body.title,
 		summary: req.body.summary,
-		body: req.body.content,
+		content: req.body.content,
 		date: new Date(),
 		author: {
 			id: authorId,
@@ -70,6 +70,43 @@ router.get('/posts/:id', async function (req, res) {
 	post.date = post.date.toISOString();
 
 	res.render('post-detail', { post: post });
+});
+
+router.get('/posts/:id/edit', async function (req, res) {
+	const postId = req.params.id;
+	const post = await db
+		.getDb()
+		.collection('posts')
+		.findOne(
+			{ _id: new ObjectId(postId) },
+			{ title: 1, summary: 1, content: 1 }
+		);
+
+	if (!post) {
+		return res.status(404).render('404');
+	}
+
+	res.render('update-post', { post: post });
+});
+
+router.post('/posts/:id/edit', async function (req, res) {
+	const postId = new ObjectId(req.params.id);
+	const result = await db
+		.getDb()
+		.collection('posts')
+		.updateOne(
+			{ _id: postId },
+			{
+				$set: {
+					title: req.body.title,
+					summary: req.body.summary,
+					content: req.body.content,
+					// date: new Date()
+				},
+			}
+		);
+
+	res.redirect('/posts');
 });
 
 module.exports = router;
